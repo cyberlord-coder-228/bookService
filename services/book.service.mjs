b8301db5a7adad14f7c36a6a7f92f13fb2a52d93
vlad <cyberlord2003@gmail.com>
2024-07-23 13:05:18 +0300

    implemented getBookById

--- services/book.service.mjs
+++ services/book.service.mjs
@@ -5,4 +5,11 @@ async function getAllBooks() {
 	return books;
 }
 
+async function getBookById(id) {
+ const books = bookLib.get();
+  const book = books.find(b => b._id === parseInt(id));
+  if (!book) throw new Error('Book not found in database');
+  return book;
+}
+
 export { getAllBooks };
